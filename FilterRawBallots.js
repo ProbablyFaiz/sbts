2eed7b49f9a55d5da42eb665fe178faa1308fc1b
Faiz Surani <faiz.surani@gmail.com>
2020-11-17 20:30:47 -0800

    Add individual ranking ballot filtering

--- FilterRawBallots.js
+++ FilterRawBallots.js
@@ -1,3 +1,29 @@
-function FILTERRAWBALLOTS(ballotIngressRange) {
-    return ballotIngressRange.filter(ballot => ballot[7] == true);
+const TEAM_READY_INDEX = 8;
+const ROWS_PER_TEAM_BALLOT = 2;
+const INDIVIDUAL_READY_INDEX = 8;
+const ROWS_PER_INDIVIDUAL_BALLOT = 8;
+
+function filterBallots(ballotIngressRange, rowsPerBallot, readyIndex) {
+    return ballotIngressRange.filter((ballot, index) => {
+        if (index === 0)
+            return false;
+        const checkboxIndex = index - ((index - 1) % rowsPerBallot);
+        return ballotIngressRange[checkboxIndex][readyIndex] === true;
+    }).map(ballot => ballot.slice(0, -1)); // Remove ready for tab row
 }
+
+function FILTERTEAMBALLOTS(ballotIngressRange) {
+    return filterBallots(ballotIngressRange, ROWS_PER_TEAM_BALLOT, TEAM_READY_INDEX);
+}
+
+function FILTERINDIVIDUALBALLOTS(ballotIngressRange) {
+    return filterBallots(ballotIngressRange, ROWS_PER_INDIVIDUAL_BALLOT, INDIVIDUAL_READY_INDEX);
+}
+
+const testArr = [
+    [0, true],
+    [1, false],
+    [2, false],
+    [3, true]
+]
+console.log(FILTERTEAMBALLOTS(testArr))
