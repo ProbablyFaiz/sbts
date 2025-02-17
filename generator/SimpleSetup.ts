5105c007429fd1fcb08f682674b9459f8b65dff4
Faiz Surani <faiz.surani@gmail.com>
2025-02-17 15:15:38 -0800

    WIP

--- generator/SimpleSetup.ts
+++ generator/SimpleSetup.ts
@@ -105,3 +105,79 @@ function SimpleSetup(tabFolderLink: string) {
     context.prelimRounds.map((r) => [r]),
   );
 }
+
+const setUpGoogleFormBallot = (
+  tabFolder: GoogleAppsScript.Drive.Folder,
+  formBallotTemplate: GoogleAppsScript.Drive.File,
+  masterSpreadsheet: GoogleAppsScript.Spreadsheet.Spreadsheet,
+  rounds: string[],
+  courtrooms: string[],
+  tournamentName: string,
+) => {
+  Logger.log("Setting up Google Form ballot...");
+  // Copy into tab folder
+  const formFile = formBallotTemplate.makeCopy(
+    `${tournamentName} Ballot`,
+    tabFolder,
+  );
+  const form = FormApp.openById(formFile.getId());
+  form.setTitle(`${tournamentName} Ballot`);
+
+  const roundItem = form
+    .getItems()
+    .find((item) => item.getTitle() === "Round #");
+  if (roundItem) {
+    const roundDropdown = roundItem.asListItem();
+    roundDropdown.setChoiceValues(rounds);
+  }
+  const courtroomItem = form
+    .getItems()
+    .find((item) => item.getTitle() === "Courtroom");
+  if (courtroomItem) {
+    const courtroomDropdown = courtroomItem.asListItem();
+    courtroomDropdown.setChoiceValues(courtrooms);
+  }
+
+  form.setDestination(
+    FormApp.DestinationType.SPREADSHEET,
+    masterSpreadsheet.getId(),
+  );
+  masterSpreadsheet
+    .getRangeByName(MasterRange.GoogleFormBallotLink)
+    .setValue(form.getPublishedUrl());
+  SpreadsheetApp.flush();
+
+  const responseSheets = masterSpreadsheet
+    .getSheets()
+    .filter((sheet) => sheet.getFormUrl());
+  Logger.log(`Found ${responseSheets.length} response sheet(s).`);
+  responseSheets.forEach((sheet) => {
+    const range = sheet.getRange(
+      1,
+      1,
+      sheet.getMaxRows(),
+      sheet.getMaxColumns(),
+    );
+    // Add alternating-color formatting
+    // range.applyRowBanding(SpreadsheetApp.BandingTheme.LIGHT_GREY, true, false);
+    sheet.setFrozenRows(1);
+    // Set a constant row height so it doesn't expand when multi-line responses are added
+    sheet.setRowHeightsForced(1, sheet.getLastRow(), 25);
+    // Set the font to 12pt Times New Roman
+    range.setFontFamily("Times New Roman");
+    range.setFontSize(12);
+    // Add Ballot PDF Link column
+    sheet.insertColumnsAfter(sheet.getLastColumn(), 1);
+    sheet.getRange(1, sheet.getLastColumn()).setValue("Ballot PDF Link");
+    // Replace "Petitioner" with "P" and "Respondent" with "R" in the header row
+    const firstRow = sheet.getRange(1, 1, 1, sheet.getLastColumn());
+    firstRow.setValues([
+      firstRow
+        .getValues()[0]
+        .map((cell) =>
+          cell.toString().replace("Petitioner", "P").replace("Respondent", "R"),
+        ),
+    ]);
+    firstRow.setFontWeight("bold");
+  });
+};
